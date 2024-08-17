193cc2065e464bbbd2aa0c45f33456ecdc836517
lukitasmois <lmoix85@gmail.com>
2024-08-16 23:12:05 -0300

    Agregue validaciones de campos.

---
+++ GamesStaticSQL/Menu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesStaticSQL
+{
+    internal class Menu
+    {
+        public static void Option1()
+        {
+            DB.GetAllGames();
+        }
+
+        public static void Option2()
+        {
+            int n1 = ValidateIntInput("Ver juegos entre (1er id): ");
+            int n2 = ValidateIntInput("Y entre (2do id): ");
+            Console.WriteLine("Ver juegos entre (1er id): ");
+            DB.GetGamesBetween(n1, n2);
+        }
+
+        public static void Option3()
+        {
+            Console.WriteLine("Ingrese el genero de los juegos a buscar:");
+            string genre = Console.ReadLine();
+            DB.GetGamesWithGenre(genre);
+        }
+
+        public static void Option4()
+        {
+            Console.WriteLine("Ingrese el nombre del juego a buscar:");
+            string name = Console.ReadLine();
+            DB.GetGameWithName(name);
+        }
+
+        public static void Option5()
+        {
+            Console.WriteLine();
+            int amount = ValidateIntInput("Ingrese la cantidad de juegos a mostrar:");
+            DB.GetGamesByQuantity(amount);
+        }
+
+        private static int ValidateIntInput(string prompt)
+        {
+            int result;
+
+            while(true)
+            {
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out result) && result > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
--- GamesStaticSQL/Program.cs
+++ GamesStaticSQL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GamesStaticSQL;
+using System.ComponentModel.Design;
 using System.Linq.Expressions;
 
 bool next= true;
@@ -8,45 +9,42 @@ while (next)
 {
     Console.Clear();
     MostrarMenu();
-    int selection = Convert.ToInt16(Console.ReadLine());
+    string input = Console.ReadLine();
+    int selection;
 
 
-    switch (selection)
+    if (int.TryParse(input, out selection))
     {
+        switch (selection)
+        {
 
-        case 1:
-            DB.GetAllGames();
-            break;
-        case 2:
-            Console.WriteLine("Ver juegos entre (1er id): ");
-            int n1 = Convert.ToInt32(Console.ReadLine());
+            case 1:
+                Menu.Option1();
+                break;
+            case 2:
+                Menu.Option2();
+                break;
 
-            Console.WriteLine("Y entre (2do id): ");
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            DB.GetGamesBetween(n1, n2);
-            break;
+            case 3:
+                Menu.Option3();
+                break;
 
-        case 3:
-            Console.WriteLine("Ingrese el genero de los juegos a buscar:");
-            string genre= Console.ReadLine();
-            DB.GetGamesWithGenre(genre);
-            break;
+            case 4:
+                Menu.Option4();
+                break;
 
-        case 4:
-            Console.WriteLine("Ingrese el nombre del juego a buscar:");
-            string name = Console.ReadLine();
-            DB.GetGameWithName(name);
-            break;
+            case 5:
+                Menu.Option5();
+                break;
 
-        case 5:
-            Console.WriteLine("Ingrese la cantidad de juegos a mostrar:");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            DB.GetGamesByQuantity(amount);
-            break;
-
-        default:
-            Console.WriteLine("{0} no existe", selection);
-            break;
+            default:
+                Console.WriteLine("La operacion: {0}, no existe", selection);
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Por favor, ingrese un número válido.");
     }
     if (next)
     {
